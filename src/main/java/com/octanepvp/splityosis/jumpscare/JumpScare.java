c4083ca2ba2dd015e1a3666bf06aab235870e3b4
SplitYoSis <87956691+SplitYoSis@users.noreply.github.com>
2023-05-04 07:12:16 +0300

    Initial commit

---
+++ src/main/java/com/octanepvp/splityosis/jumpscare/JumpScare.java
@@ -0,0 +1,17 @@
+package com.octanepvp.splityosis.jumpscare;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class JumpScare extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
